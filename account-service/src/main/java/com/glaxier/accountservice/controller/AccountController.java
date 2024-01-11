c5bdaae9877c801e35a028513e0c83214f69934f
Tahir <tahirclk@hotmail.com>
2024-01-11 23:44:18 +0300

    feat: added api gateway

--- account-service/src/main/java/com/glaxier/accountservice/controller/AccountController.java
+++ account-service/src/main/java/com/glaxier/accountservice/controller/AccountController.java
@@ -3,10 +3,7 @@ package com.glaxier.accountservice.controller;
 import com.glaxier.accountservice.model.Account;
 import com.glaxier.accountservice.service.AccountService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/accounts")
